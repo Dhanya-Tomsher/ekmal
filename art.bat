d3da7e6cc401a3c7554e150633125215fbe48f8e
Dhanya-Tomsher <dhanya@tomsher.com>
2023-12-11 10:27:54 +0400

    Test

--- art.bat
+++ art.bat
@@ -7,4 +7,4 @@ IF "%1"=="m"   start /WAIT /B php artisan migrate
 IF "%1"=="mb"  start /WAIT /B php artisan migrate:rollback
 IF "%1"=="m:M" start /WAIT /B php artisan make:model %_tail%
 IF "%1"=="m:C" start /WAIT /B php artisan make:controller %_tail%
-IF "%1"=="m:m" start /WAIT /B php artisan make:migration %_tail%
+IF "%1"=="m:m" start /WAIT /B php artisan make:migration %_tail% 
